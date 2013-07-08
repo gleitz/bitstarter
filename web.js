73f6a7ead48bfe9aa37395f7c3dbbea772a258e1
Benjamin Gleitzman <gleitz@mit.edu>
2013-07-08 13:21:27 -0400

    Serving static HTML

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
-var express = require('express');
+/*global require console process*/
+var express = require('express'),
+    fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    response.send(fs.readFileSync('index.html', 'utf8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });

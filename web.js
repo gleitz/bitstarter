c3b992db7fa9c5bee028d70edb6a10700c79ab2d
Benjamin Gleitzman <gleitz@mit.edu>
2013-07-17 13:03:40 -0700

    Added initial fantasy movie information

--- web.js
+++ web.js
@@ -1,11 +1,21 @@
-/*global require console process*/
+/*global require console process __dirname*/
 var express = require('express'),
-    fs = require('fs');
+    fs = require('fs'),
+    lessMiddleware = require('less-middleware');
 
 var app = express.createServer(express.logger());
+var oneDay = 86400000;
 
 app.get('/', function(request, response) {
     response.send(fs.readFileSync('index.html', 'utf8'));
+});
+
+app.configure(function(){
+    app.use(lessMiddleware({
+        src      : __dirname + "/public",
+        compress : true
+    }));
+    app.use(express['static'](__dirname + '/public', { maxAge: oneDay }));
 });
 
 var port = process.env.PORT || 5000;
